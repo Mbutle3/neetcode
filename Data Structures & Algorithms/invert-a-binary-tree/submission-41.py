275931bfda5d5fb6b552bbb16b1f919f8185a1bc
Michael Antonio Butler <56573114+Mbutle3@users.noreply.github.com>
2026-04-21 11:29:34 -0400

    Add: invert-a-binary-tree - submission-41

---
+++ Data Structures & Algorithms/invert-a-binary-tree/submission-41.py
@@ -0,0 +1,21 @@
+from collections import deque
+
+class Solution:
+    def invertTree(self, root: Optional[TreeNode]) -> Optional[TreeNode]:        
+        def dfs(root):
+            if not root:
+                return None
+            # swap left and right subtrees
+            root.left, root.right = root.right, root.left
+            # recurse into each subtree
+            dfs(root.left)
+            dfs(root.right)
+            return root
+        
+        def bfs(root):
+            q = deque([root])
+            while q:
+                for _ in range(len(q)):
+                    curr = q.popleft()
+                    curr.left, curr.right = curr.right, curr.left
+        return bfs(root)
