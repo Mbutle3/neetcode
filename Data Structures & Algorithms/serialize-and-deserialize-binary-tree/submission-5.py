d903d8a4c028fd946d95fa8a7bffe66214e277ae
Michael Antonio Butler <56573114+Mbutle3@users.noreply.github.com>
2026-04-24 12:41:55 -0400

    Add: serialize-and-deserialize-binary-tree - submission-5

---
+++ Data Structures & Algorithms/serialize-and-deserialize-binary-tree/submission-5.py
@@ -0,0 +1,53 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+
+class Codec:
+    
+    # Encodes a tree to a single string.
+    def serialize(self, root: Optional[TreeNode]) -> str:
+        # BFS
+
+        res = []
+        queue = collections.deque([root])
+        while queue:
+            node = queue.popleft()
+            if node:
+                res.append(str(node.val))
+                queue.append(node.left)
+                queue.append(node.right)
+            else:
+                res.append("N")
+        return ",".join(res)
+
+        
+        
+
+        
+    # Decodes your encoded data to tree.
+    def deserialize(self, data: str) -> Optional[TreeNode]:
+        # BFS Mirror Image
+        if data == 'N':
+            return None
+
+        vals = data.split(',')
+        root = TreeNode(int(vals[0]))
+        queue = collections.deque([root])
+        i = 1
+
+        while queue:
+            node = queue.popleft()
+            if vals[i] != 'N':
+                node.left = TreeNode(int(vals[i]))
+                queue.append(node.left)
+            i += 1
+            if vals[i] != 'N':
+                node.right = TreeNode(int(vals[i]))
+                queue.append(node.right)
+            i += 1
+
+        return root
+                
