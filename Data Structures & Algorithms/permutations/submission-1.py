3a63cac583a8317c94948bd2058f8dd9d759e1e6
Michael Antonio Butler <56573114+Mbutle3@users.noreply.github.com>
2026-04-29 18:14:57 -0400

    Add: permutations - submission-1

---
+++ Data Structures & Algorithms/permutations/submission-1.py
@@ -0,0 +1,15 @@
+class Solution:
+    def permute(self, nums: List[int]) -> List[List[int]]:
+        res = []
+        def dfs(i, perms):
+            if i > len(nums):
+                return
+            if i == len(nums):
+                res.append(perms)
+            perms.append(nums[i])
+            dfs(i, perms)
+            perms.pop()
+            dfs(i + 1, perms)
+        dfs(0, [])
+        return res
+        
