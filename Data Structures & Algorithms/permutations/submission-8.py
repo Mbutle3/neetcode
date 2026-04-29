7cd63525ac103c03e4fced6e7f05170d04f862af
Michael Antonio Butler <56573114+Mbutle3@users.noreply.github.com>
2026-04-29 18:44:33 -0400

    Add: permutations - submission-8

---
+++ Data Structures & Algorithms/permutations/submission-8.py
@@ -0,0 +1,18 @@
+class Solution:
+    def permute(self, nums: List[int]) -> List[List[int]]:
+        res = []
+        def dfs(perms):
+
+            if len(perms) == len(nums):
+                res.append(perms.copy())
+                return
+
+            for j in range(len(nums)):
+                if nums[j] not in perms:
+                    perms.append(nums[j])
+                    dfs(perms)
+                    perms.pop()
+                    
+        dfs([])
+        return res
+        
