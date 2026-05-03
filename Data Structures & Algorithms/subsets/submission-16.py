1fbd09dd1420a701caff6035a72bc24c380bc50e
Michael Antonio Butler <56573114+Mbutle3@users.noreply.github.com>
2026-05-03 11:18:01 -0400

    Add: subsets - submission-16

---
+++ Data Structures & Algorithms/subsets/submission-16.py
@@ -0,0 +1,16 @@
+class Solution:
+    def subsets(self, nums: List[int]) -> List[List[int]]:
+        res = []
+        subsets = []
+
+        def backtack(i):
+            res.append(subsets.copy())
+
+            for j in range(i, len(nums)):
+                subsets.append(nums[j])
+                backtack(j + 1)
+                subsets.pop()
+        backtack(0)
+        return res
+
+        
