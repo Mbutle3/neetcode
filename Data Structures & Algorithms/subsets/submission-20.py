f84f9279093abdf6b1a207dc034ecd45b9a5a4ec
Michael Antonio Butler <56573114+Mbutle3@users.noreply.github.com>
2026-05-03 13:04:47 -0400

    Add: subsets - submission-20

---
+++ Data Structures & Algorithms/subsets/submission-20.py
@@ -0,0 +1,15 @@
+class Solution:
+    def subsets(self, nums: List[int]) -> List[List[int]]:
+        res = []
+        subset = []
+
+        def backTrack(i):
+            res.append(subset.copy())
+
+            for j in range(len(nums)):
+                subset.append(nums[j])
+                backTrack(i + 1)
+                subset.pop()
+        backTrack(0)
+        return res
+        
