4a2ae22b99ee6358657e9c4fde9d5463c27a966e
Michael Antonio Butler <56573114+Mbutle3@users.noreply.github.com>
2026-05-03 12:58:19 -0400

    Add: combination-target-sum - submission-12

---
+++ Data Structures & Algorithms/combination-target-sum/submission-12.py
@@ -0,0 +1,21 @@
+class Solution:
+    def combinationSum(self, nums: List[int], target: int) -> List[List[int]]:
+        res = []
+        combos = []
+
+        def backTrack(i, total):
+            if total == target:
+                res.append(combos.copy())
+                return
+            
+            if total > target or i >= len(nums):
+                return
+
+            for j in range(i, len(nums)):
+                combos.append(nums[j])
+                backTrack(j, total + nums[j])
+                combos.pop()
+        backTrack(0, 0)
+        return res
+
+        
