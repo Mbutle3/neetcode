2054a91ecd6dbada227fc66fd737712faecfed92
Michael Antonio Butler <56573114+Mbutle3@users.noreply.github.com>
2026-08-01 13:03:29 -0400

    Add: same-binary-tree - submission-27

---
+++ Data Structures & Algorithms/same-binary-tree/submission-27.py
@@ -0,0 +1,22 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+
+class Solution:
+    def isSameTree(self, p: Optional[TreeNode], q: Optional[TreeNode]) -> bool:
+        arr1 = []
+        arr2 = []
+        def inOrder(x, arr):
+            if x is None:
+                return 
+            inOrder(x.left)
+            arr.append(x.val)
+            inOrder(x.right)
+        
+        inOrder(p, arr1)
+        inOrder(q, arr2)
+
+        return arr1 == arr2        
